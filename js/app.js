1d1aaad173e976e2115e62ceb38d4fdc6689d881
Agustin Macko <mackoaa@gmail.com>
2025-01-31 14:29:30 -0300

    app section donde

--- js/app.js
+++ js/app.js
@@ -2,4 +2,4 @@ import "../components/button/button.js";
 import "../components/text-block/text-block.js";
 import "../components/icon-text/icon-text.js";
 import "../components/benefit-block/benefit-block.js";
-import "../components/tex-image-block/text-image-block.js";
+import "../components/app-section/app-section.js";
