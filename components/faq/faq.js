c533a7236ef4eec491b0c939b85408b755fc7c7a
Agustin Macko <mackoaa@gmail.com>
2025-02-03 23:19:59 -0300

    url git

--- components/faq/faq.js
+++ components/faq/faq.js
@@ -6,25 +6,16 @@ class FAQItem extends HTMLElement {
     connectedCallback() {
         const question = this.getAttribute("question");
         const answer = this.innerHTML;
-        // locasl this.innerHTML = `
-        //     <div class="faq-item">
-        //         <div class="faq-question">
-        //             <span class="p-16 regular">${question}</span>
-        //             <img src="/assets/icons/plus.svg" class="faq-icon">
-        //         </div>
-        //         <div class="faq-answer small-text thin c-grey ">${answer}</div>
-        //     </div>
-        // `;
-
-        this.innerHTML = `
-        <div class="faq-item">
-            <div class="faq-question">
-                <span class="p-16 regular">${question}</span>
-                <img src="https://mackoagustin.github.io/tracker/assets/icons/plus.svg" class="faq-icon">
+         this.innerHTML = `
+            <div class="faq-item">
+                <div class="faq-question">
+                    <span class="p-16 regular">${question}</span>
+                    <img src=" ./assets/icons/plus.svg" class="faq-icon">
+                </div>
+                <div class="faq-answer small-text thin c-grey ">${answer}</div>
             </div>
-            <div class="faq-answer small-text thin c-grey ">${answer}</div>
-        </div>
-    `;
+        `;
+;
 
         const item = this.querySelector(".faq-item");
         const icon = this.querySelector(".faq-icon");
@@ -35,7 +26,7 @@ class FAQItem extends HTMLElement {
             document.querySelectorAll("faq-item .faq-item").forEach(faq => {
                 faq.classList.remove("active");
                 // local faq.querySelector(".faq-icon").src = "/assets/icons/plus.svg";
-                faq.querySelector(".faq-icon").src = "https://mackoagustin.github.io/tracker/assets/icons/plus.svg";
+                faq.querySelector(".faq-icon").src = "./assets/icons/plus.svg";
 
                
             });
@@ -43,7 +34,7 @@ class FAQItem extends HTMLElement {
             if (!isActive) {
                 item.classList.add("active");
                 // local icon.src = "/assets/icons/less.svg";
-                icon.src = " https://mackoagustin.github.io/tracker/assets/icons/less.svg";
+                icon.src = "./assets/icons/less.svg";
             }
         });
     }
