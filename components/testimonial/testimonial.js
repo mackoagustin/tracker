8ab7ee1f5af3894a48481ca6b912a9a3825c6f3f
Agustin Macko <mackoaa@gmail.com>
2025-03-17 09:22:39 -0300

    M - slider mobile

--- components/testimonial/testimonial.js
+++ components/testimonial/testimonial.js
@@ -10,7 +10,24 @@ class Testimonial extends HTMLElement {
             const name = this.getAttribute("name") || "";
             const role =  this.getAttribute("role") || "";
             const idTestimonial = this.getAttribute('idTestimonial');
-            this.innerHTML = `
+
+            const isDesktop = window.matchMedia("(min-width: 768px)").matches;
+            if(!isDesktop){
+                this.innerHTML = `
+                <article class="testimony-container pt-5">
+                    <div class="testimony-background-shape"></div>
+                        <div class="testimony-card pt-5 pb-5" id="${idTestimonial}"    >
+                            <p class="p-16 thin t-center testimony-body  fs-32px ">${text}</p>
+                            <div class="pt-4 testimonial-flex">
+                            <img src="${image}" height="70" width="70" alt="">
+                            <p class="medium-large-text m-0 ">${name}</p>
+                            <p class="medium-text light c-grey m-0">${role}</p>
+                        </div>
+                    </div>
+                </article>
+            `
+            } else {
+                this.innerHTML = `
                 <article class="testimony-container pt-5">
                     <div class="testimony-background-shape"></div>
                         <div class="testimony-card pt-5 pb-5" id="${idTestimonial}"    >
@@ -23,6 +40,8 @@ class Testimonial extends HTMLElement {
                     </div>
                 </article>
             `
+            }
+           
     }
 }
 
